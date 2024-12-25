eed0da8bb4e2a3b2648192aefbe7a739a021965a
Lo <l709937065@gmail.com>
2024-12-25 10:27:48 +0800

    feat(ui): add Button example

---
+++ playground/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Button } from "@taskon/ui"
+import { ref } from "vue"
+
+// TODO better type support
+const variant = ref("default")
+const size = ref("default")
+</script>
+
+<template>
+  <!-- @vue-expect-error TODO types export -->
+  <Button :variant="variant" :size="size">btn text</Button>
+  <div>
+    variants:
+    <button @click="variant = 'default'">default</button>
+    <button @click="variant = 'outline'">outline</button>
+    <button @click="variant = 'secondary'">secondary</button>
+  </div>
+  <div>
+    sizes
+    <button @click="size = 'default'">default</button>
+    <button @click="size = 'sm'">sm</button>
+    <button @click="size = 'lg'">lg</button>
+  </div>
+</template>
