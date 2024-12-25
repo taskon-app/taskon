26643ee2bcf2c743f663bec96820496ffd306225
Lo <l709937065@gmail.com>
2024-12-25 10:28:48 +0800

    feat(ui): add Button example (#1)

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,45 @@
+import { readFileSync, writeFileSync } from "node:fs"
+import { fileURLToPath, URL } from "node:url"
+import vue from "@vitejs/plugin-vue"
+import uno from "unocss/vite"
+import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    dts({ tsconfigPath: "./tsconfig.json" }),
+    uno(),
+    {
+      // must mark as `@unocss-include`
+      name: "taskon-ui-auto-gen-unocss-include",
+      closeBundle() {
+        ;["index.js", "index.cjs"].forEach((file) => {
+          const path = `./dist/${file}`
+          const content = readFileSync(path, "utf-8")
+          writeFileSync(path, `// @unocss-include\n\n${content}`)
+        })
+      },
+    },
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: {
+        index: "src/index.ts",
+      },
+    },
+    sourcemap: true,
+    rollupOptions: {
+      output: [
+        { format: "es", globals: { vue: "Vue" }, exports: "named" },
+        { format: "cjs", globals: { vue: "Vue" }, exports: "named" },
+      ],
+      external: ["vue"],
+    },
+  },
+})
